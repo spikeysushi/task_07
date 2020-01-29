3a51c2770cfe279a6349a64637e042b30cdcef68
Omar Al Shayji <omaralshayji@Omars-MacBook-Pro.local>
2020-01-29 11:47:41 +0300

    first commit done

--- restaurants/views.py
+++ restaurants/views.py
@@ -31,10 +31,22 @@ def restaurant_create(request):
     return render(request, 'create.html', context)
 
 def restaurant_update(request, restaurant_id):
-
-    return
+    restaurant = Restaurant.objects.get(id=restaurant_id)
+    form = RestaurantForm(instance= restaurant)
+    if request.method == "POST":
+        form = RestaurantForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('restaurant-list')
+    context = {
+        "form":form,
+        "restaurant":restaurant
+    }
+    return render(request, "update.html", context)
 
 def restaurant_delete(request, restaurant_id):
+    Restaurant.objects.get(id=restaurant_id).delete()
+    return redirect("restaurant-list")
 
 
-    return
+
